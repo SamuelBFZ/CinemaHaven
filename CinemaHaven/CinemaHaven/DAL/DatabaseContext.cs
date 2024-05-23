85a3af56c378e55f4771c432cba5c5936e441a2e
SAMUEL BEDOYA FLOREZ <samuelbedoya309347@correo.itm.edu.co>
2024-05-22 20:05:36 -0500

    Database Connection configured

---
+++ CinemaHaven/CinemaHaven/DAL/DatabaseContext.cs
@@ -0,0 +1,44 @@
+﻿using CinemaHaven.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CinemaHaven.DAL
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<PaymentMethod> PaymentMethods { get; set; }
+        public DbSet<Pay> Pays { get; set; }
+        public DbSet<Bill> Bills { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.paymentMethods)
+                .WithOne(p => p.User)
+                .HasForeignKey(p => p.UserId);
+
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.bills)
+                .WithOne(p => p.User)
+                .HasForeignKey(p => p.UserId);
+
+            modelBuilder.Entity<Movie>()
+                .HasMany(p => p.pays)
+                .WithOne(p => p.Movie)
+                .HasForeignKey(p => p.MovieId);
+
+            modelBuilder.Entity<PaymentMethod>()
+                .HasMany(pm => pm.pays)
+                .WithOne(p => p.PaymentMethod)
+                .HasForeignKey(p => p.PaymentMethodId);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
--- CinemaHaven/CinemaHaven/Program.cs
+++ CinemaHaven/CinemaHaven/Program.cs
@@ -1,8 +1,17 @@
+using CinemaHaven.DAL;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<DatabaseContext>(o =>
+{
+    o.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
