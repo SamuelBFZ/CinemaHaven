2fb639a40ed85cc0006f8e599e657c201e116703
SAMUEL BEDOYA FLOREZ <samuelbedoya309347@correo.itm.edu.co>
2024-06-11 18:41:13 -0500

    Create for paymentMethods created but not implementated

---
+++ CinemaHaven/CinemaHavenWEB/CinemaHavenWEB/Controllers/PayMethodsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CinemaHavenWEB.Controllers
+{
+    public class PayMethodsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+    }
+}
